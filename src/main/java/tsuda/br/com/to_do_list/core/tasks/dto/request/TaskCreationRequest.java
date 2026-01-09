c550a2f027368c06502529ed0e2d8769926cd6ff
Tsuda12 <ftsuda12@gmail.com>
2026-01-09 14:02:23 -0300

    feat: adjust local date time in swagger

--- src/main/java/tsuda/br/com/to_do_list/core/tasks/dto/request/TaskCreationRequest.java
+++ src/main/java/tsuda/br/com/to_do_list/core/tasks/dto/request/TaskCreationRequest.java
@@ -1,5 +1,6 @@
 package tsuda.br.com.to_do_list.core.tasks.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import tsuda.br.com.to_do_list.utils.MessageUtils;
@@ -14,8 +15,10 @@ public record TaskCreationRequest(
         String description,
 
         @NotNull(message = MessageUtils.STARTED_AT_REQUIRED)
+        @Schema(example = "2026-01-09T13:30:00", type = "string", format = "date-time")
         LocalDateTime startedAt,
 
         @NotNull(message = MessageUtils.ENDED_AT_REQUIRED)
+        @Schema(example = "2026-01-09T14:30:00", type = "string", format = "date-time")
         LocalDateTime endedAt
 ) {}
